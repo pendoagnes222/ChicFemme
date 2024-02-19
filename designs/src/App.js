fcc599f64728c2b6e7290d97df33d114dc60d737
nthenyakyalo202 <nthenyakyalo202@gmail.com>
2024-02-19 21:41:17 +0300

    design

---
+++ designs/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      hi
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
+    </div>
+  );
+}
+
+export default App;
---
+++ designs/src/Components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import { useRef } from 'react'
+import {FaBars, FaTimes} from "react-icons/fa"
+import "../Navbar/Navbar.css"
+
+const Navbar = () => {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+  return (
+    <div>
+      <header>
+        <h3>Logo</h3>
+        <nav ref={navRef}>
+          <a href='/#'>Home</a>
+          <a href='/#'>New In</a>
+          <a href='/#'>Brands</a>
+          <a href='/#'>About Us</a>
+          <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+        <FaTimes />
+      </button>
+        </nav>
+
+        <button className='nav-btn' onClick={showNavbar}>
+        <FaBars />
+      </button>
+      </header>
+      
+    </div>
+  )
+}
+
+export default Navbar
